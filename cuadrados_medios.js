a56e013cec26e802d0b37e8a956587ab92a53238
ccarlosuarezz <karlosuarez710@gmail.com>
2021-11-01 12:07:08 -0500

    Add functions for get means quares, get new random number and show the result

--- cuadrados_medios.js
+++ cuadrados_medios.js
@@ -1,14 +1,28 @@
 const semilla = document.getElementById('input_semilla');
 const button = document.getElementById('button');
+const MIN_SEED_LENGTH = 4;
 
 button.addEventListener("click", generate);
 
 function generate() {
     const result = document.createElement('p');
-    result.textContent = semilla.value;
+    result.textContent = newRandomNumber(semilla.value);
     document.body.appendChild(result);
 }
 
-function getMeanSquares(int) {
+function newRandomNumber(seed) {
+    const seedLength = `${seed}`.length;
+    if (seedLength >= MIN_SEED_LENGTH)  {
+        let meanSquares = getMeanSquares(seed, seedLength);
+        return meanSquares / Math.pow(10, seedLength);
+    } else {
+        window.alert('El numero ingresado tiene menos de 4 digitos');
+    }
+}
 
+function getMeanSquares(seed, seedLength) {
+    let squaredSeed = seed * seed;
+    let squaredSeedLength = `${squaredSeed}`.length;
+    let division = Math.round(squaredSeedLength/4);
+    return `${squaredSeed}`.substr(division, seedLength);
 }
